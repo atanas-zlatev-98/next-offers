7c435722fa89296c291e4d15aa264545d702ab30
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-03-23 20:04:30 +0200

    root layout

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div className="flex h-full items-center justify-center">
+            <h1 className="text-4xl font-bold text-white">Welcome to Next Offers!</h1>
+        </div>
+    )
+}

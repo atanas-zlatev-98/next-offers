b741cee60827099db1f7718a2104d9f32086643f
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-03-23 20:19:00 +0200

    shadcn dialog popup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import { AddProductDialog } from "./add-product/AddProductDialog";
+
 export default function Home(){
+
     return (
         <div className="flex h-full items-start p-10 justify-center bg-white">
-            <h1 className="text-4xl font-bold text-black">Welcome to Next Offers!</h1>
-            
+            <h1 className="text-4xl font-bold text-black">Next Offers</h1>
+            <AddProductDialog/>
         </div>
     )
 }
